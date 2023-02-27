fa8debf7f78fe7e8bcc707147143fba49c975281
Kashapova Leisyan <LeisyanRA@mail.ru>
2023-02-27 22:58:20 +0500

    seminar4

--- src/main/java/org/example/seminar4/hw/Zadanie2.java
+++ src/main/java/org/example/seminar4/hw/Zadanie2.java
@@ -1,33 +1,35 @@
 package org.example.seminar4.hw;
 
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.LinkedList;
 
 public class Zadanie2 {
     public static void main(String[] args) {
-        LinkedList<String> list = new LinkedList<>(Arrays.asList("(",")","[","]","{","}"));
-        String s = "(){}[]";
+        String s = "({([)}])[]";
         System.out.println(isValid(s));
-//        LinkedList<String> newList = new LinkedList<>();
-//        for (int i = 0; i < list.size(); i++) {
-//            if (isIn(newList, list.get(i)) != -1) {}
-//        }
     }
 
     public static boolean isValid(String s) {
+        LinkedList<Character> newList = new LinkedList<>();
         boolean flag = true;
-        for (int i = 0; i < s.length(); i++) {
-            if ("]})".indexOf(s.charAt(i)) != -1){  //contains("{")){  //s.charAt(i))) {
-                System.out.println(1);
-                flag = true;
+        int i = 0;
+        while (i < s.length() && flag) {
+            Character ch = s.charAt(i);
+            if ("]})".indexOf(ch) != -1) {  // есть закрывающая скобка
+                if (newList.isEmpty()) flag = false;
+                else {
+                    if (ch.equals(']') && newList.get(newList.size() - 1).equals('[')) {
+                        newList.remove(newList.size() - 1);
+                    } else if (ch.equals('}') && newList.get(newList.size() - 1).equals('{')) {
+                        newList.remove(newList.size() - 1);
+                    } else if (ch.equals(')') && newList.get(newList.size() - 1).equals('(')) {
+                        newList.remove(newList.size() - 1);
+                    } else return false;
+                }
+            } else {
+                newList.add(ch);
             }
-            else flag = false;
-            
+            i++;
         }
         return flag;
     }
-//    public static Integer isIn(Collection listIn, String simb) {
-//        if (simb
-//    }
 }
