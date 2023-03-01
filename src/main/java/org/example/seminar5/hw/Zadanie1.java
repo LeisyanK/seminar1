51dd5d86a78340cb5e8aae620f4f1463d2e23102
Kashapova Leisyan <LeisyanRA@mail.ru>
2023-03-01 11:52:05 +0500

    seminar5

--- src/main/java/org/example/seminar5/hw/Zadanie1.java
+++ src/main/java/org/example/seminar5/hw/Zadanie1.java
@@ -21,7 +21,6 @@ public class Zadanie1 {
             System.out.println("Error: " + e);
         } finally {
             try {
-//                assert br != null;
                 if(br != null) br.close();
             } catch(IOException e) {
                 System.out.println("Error: " + e);
@@ -35,10 +34,7 @@ public class Zadanie1 {
     public static Map<String, Integer> findWords(String str) {
         Map<String, Integer> newMap = new HashMap<>();
         List<String> myList = new ArrayList<>(List.of(str.split(" ")));
-//        List<String> myList = new ArrayList<>();      //  строка 16 заменяет строки 17 и 18
-//        myList.addAll(List.of(str.split(" ")));
         for (int i = myList.size() - 1; i >= 0; i--) {
-            // обработку кавычек не стала добавлять
             if (myList.get(i).contains(").") ||
                     myList.get(i).contains(")!") ||
                     myList.get(i).contains(")?") ||
@@ -71,6 +67,7 @@ public class Zadanie1 {
             newMap.putIfAbsent(s.toLowerCase(), 0);
             newMap.put(s.toLowerCase(), newMap.get(s.toLowerCase()) + 1);
         }
+//        System.out.println(myList);
         return newMap;
     }
 }
